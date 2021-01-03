bd06361759b6f2f9891627a48ae33726e86a701c
Johanna Horn <j.von.horn@hotmail.com>
2021-01-03 16:02:08 +0100

    navbar layout + init button

--- assignement-jvnhrn/src/components/Navbar.js
+++ assignement-jvnhrn/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
-
-
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -13,10 +12,13 @@ function Navbar() {
     return (
         <>
         <nav className ="navbar"></nav>
-        <div className="navbar-container"></div>
-            <Link className="navbar-logo" to="/" onClick={handleClick}>jvnhrn</Link>
-
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <div className="navbar-container">
+            <ul className="nav-menu">
+                <li className='nav-logo'>
+                    <Link className='nav-logo' onClick={handleClick} to="/">
+                        jvnhrn
+                    </Link>
+                </li>
                 <li className='nav-item'>
                     <Link className='nav-links' onClick={handleClick, closeMenu} to="/Assignement"> 
                     Assignement
@@ -37,7 +39,9 @@ function Navbar() {
                         Why
                     </Link>
                 </li>
-        </ul>
+            </ul>
+        </div>
+
         </>
     )
 }
